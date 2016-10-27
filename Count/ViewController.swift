fd3bdace682ea25d1d73a08c6df45cf3e31efe58
Sakaue Aoi <aoiue.w.bbb@gail.com>
2016-10-27 23:55:49 +0900

    カウントアプリ

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -10,9 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number : Int = 0
+    @IBOutlet var label : UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        
+        if number >= 10 {
+            label.textColor = UIColor.brown
+        }else if number <= -10{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.darkGray
+        }
+    }
+    
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+        
+        if number >= 10 {
+            label.textColor = UIColor.brown
+        }else if number <= -10{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.darkGray
+        }
+
+    }
+    @IBAction func kakeru(){
+        number = number * 2
+        label.text = String(number)
+        
+        if number >= 10 {
+            label.textColor = UIColor.brown
+        }else if number <= -10{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.darkGray
+        }
+
+    }
+
+    @IBAction func waru(){
+        number = number / 2
+        label.text = String(number)
+        
+        if number >= 10 {
+            label.textColor = UIColor.brown
+        }else if number <= -10{
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.darkGray
+        }
+
+    }
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
     }
 
     override func didReceiveMemoryWarning() {
