e645947134b6c99ea7187f679fe913f255260343
Sakaue Aoi <aoiue.w.bbb@gail.com>
2016-11-01 14:57:35 +0900

    カウントアプリ

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
     @IBAction func plus(){
         number = number + 1
         label.text = String(number)
